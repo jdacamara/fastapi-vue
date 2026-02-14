5148a070406c4ba3b86fcb841c80ee7d1e37e837
Jehan Da Camara <jehandacamara@Jehans-MacBook-Pro.local>
2026-02-15 00:31:20 +0100

    feat: add environment variable for the cors origins

--- services/backend/src/backend/main.py
+++ services/backend/src/backend/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from src.backend.config import settings
 
-
+# Create FastAPI app
 app = FastAPI()
 
+# Adjust the CORS to accept from the front end
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=settings.front_end_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
